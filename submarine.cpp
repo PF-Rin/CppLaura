1c4f6cf5d9baf257904b6d0a5c419637d1cb0208
Spencer Whitehead <spencer.whitehead@rogers.com>
2014-10-05 09:33:52 -0400

    Added empty constructor

--- submarine.cpp
+++ submarine.cpp
@@ -1,5 +1,9 @@
 #include "submarine.h"
 
+Submarine::Submarine() {
+  //Iniziationsoaitasnlndaudujsfrhoweanfj.dcnsxz,  
+}
+
 Submarine::Submarine(std::string submarine_name) {
   //Initializations and whatever goes here
 }
