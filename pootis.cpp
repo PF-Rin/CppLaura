9622f2a780dc2a6b7ab6ba1a34868d9b5b65b526
Spencer Whitehead <spencer.whitehead@rogers.com>
2014-10-06 09:45:14 -0400

    Create pootis.cpp

---
+++ pootis.cpp
@@ -0,0 +1,15 @@
+#define pootis iostream
+
+#include <pootis>
+
+#define pootis int main () {
+
+pootis
+
+#define pootis std::cout << "Hello World" << std::endl;
+
+pootis
+
+#define pootis }
+
+pootis
