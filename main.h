0be74c0f1ef3c7b6537f4e13983aa8ba650ef10b
Spencer Whitehead <spencer.whitehead@rogers.com>
2014-11-10 21:22:45 -0500

    Create main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <iostream>
+#include "laura.h"
+#include "submarine.h"
+#include "planet.h"
+
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "SDL2main.lib")
+#pragma comment(lib, "SDL2_image.lib")
+#pragma comment(lib, "SDL2_ttf.lib")
+
+#endif
